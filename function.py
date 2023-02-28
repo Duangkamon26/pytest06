b21ff21b956fa784fcd5044e04ff4360b4d2211c
Duangkamon26 <115264230+Duangkamon26@users.noreply.github.com>
2023-02-28 13:38:16 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,36 @@
+def vaildate_number(number):
+    if type(number) != int:
+        if type(number) == str:
+            return "input integer"
+        elif number >= 1 and number <= 12:
+            return "input integer"
+        else:
+            return "out of range"
+    elif number >= 1 and number <= 12 :
+        return number
+    else:
+        return "out of range"
+
+def number_to_month(number):
+    month = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december"
+        ]
+    return month[number-1]
+
+def display_month(number):
+    result = vaildate_number(number)
+    if type(result) == int:
+        return number_to_month(result)
+    else:
+        return result
